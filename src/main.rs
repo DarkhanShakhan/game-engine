83a820b670ca509135a1f0c14e8c7c9e4fba02ee
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2023-09-18 14:49:43 +0600

    adds second bot

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,39 @@ fn main() {
 }
 
 fn simulate() {
-    let mut bot = Bot::new("./bot2/bot");
+    let mut bot_2 = Bot::new("./bot2/bot");
+    let mut bot_1 = Bot::new("./bot1/bot.py");
     let mut board = Board::default();
     board.set_city_owner("400-100", city::Owner::Player("p1".to_string()));
-    board.current_player = "p1".to_string();
-    for _ in 0..50 {
-        bot.send_board(&board.to_string());
+    board.set_city_owner("600-300", Owner::Player("p2".to_string()));
+
+    for _tick in 0..78 {
+        // if tick % 2 == 0 {
+        board.current_player = "p1".to_string();
+        bot_1.send_board(&board.to_string());
         println!("{board}");
-        if let Some(coords) = bot.get_move() {
+        if let Some(coords) = bot_1.get_move() {
+            println!("{:?}", coords);
             board.add_move(
                 Owner::Player("p1".to_string()),
                 &format!("{}-{}", coords.0, coords.1),
                 &format!("{}-{}", coords.2, coords.3),
             );
         };
+        // } else {
+        board.current_player = "p2".to_string();
+        bot_2.send_board(&board.to_string());
+        // println!("{board}");
+        if let Some(coords) = bot_2.get_move() {
+            println!("{:?}", coords);
+            board.add_move(
+                Owner::Player("p2".to_string()),
+                &format!("{}-{}", coords.0, coords.1),
+                &format!("{}-{}", coords.2, coords.3),
+            );
+            // };
+        }
+
         board.tick();
     }
 }
