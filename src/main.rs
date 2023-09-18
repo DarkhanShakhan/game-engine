ab663a5b95eeac4aaa798797e66338f78a23c585
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2023-09-18 08:50:03 +0600

    adds ui

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 mod board;
 mod bot;
 mod city;
+mod game;
 mod movement;
+use std::{error::Error, time::Duration};
+
 use board::Board;
 use bot::Bot;
-
+use ui::crossterm::run;
+mod ui;
 use crate::city::Owner;
 
-fn main() {
-    simulate()
+fn main() -> Result<(), Box<dyn Error>> {
+    run(Duration::from_millis(45))?;
+    Ok(())
 }
 
 fn simulate() {
@@ -17,8 +22,9 @@ fn simulate() {
     board.set_city_owner("400-100", city::Owner::Player("p1".to_string()));
     board.current_player = "p1".to_string();
     for _ in 0..50 {
+        board.ascii();
         bot.send_board(&board.to_string());
-        println!("{board}");
+        // println!("{board}");
         if let Some(coords) = bot.get_move() {
             board.add_move(
                 Owner::Player("p1".to_string()),
